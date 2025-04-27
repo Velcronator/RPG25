a6d12403e98e159f31a30e9404046f45a5000b00
velcronator <valacos7@gmail.com>
2025-04-27 17:44:00 +1000

    5.3 Swappable Control Systems: and fixed up bug where the enemy didn't have the actionscheduler.cs

--- Assets/Scripts/Core/ActionScheduler.cs
+++ Assets/Scripts/Core/ActionScheduler.cs
@@ -4,12 +4,6 @@ namespace RPG.Core
 {
     public class ActionScheduler : MonoBehaviour
     {
-        // This class is responsible for scheduling and executing actions in the game.
-        // It can be used to manage the execution of various actions, such as animations, events, etc.
-        // Add your action scheduling logic here.
-        // For example, you might want to add methods to start, stop, or queue actions.
-        // Example method to start an action
-
         IAction currentAction;
 
         public void StartAction(IAction action)
