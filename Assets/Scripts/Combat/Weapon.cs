2de601adedc8f8733cac9a84e25d666da95a410e
velcronator <valacos7@gmail.com>
2025-05-07 00:42:32 +1000

    10.5 Unarmed As A Weapon

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -6,11 +6,21 @@ namespace RPG.Combat
     public class Weapon : ScriptableObject
     {
         [SerializeField] AnimatorOverrideController animatorOverride = null;
-        [SerializeField] GameObject weaponPrefab = null;
+        [SerializeField] GameObject equippedPrefab = null;
+        [SerializeField] float weaponDamage;
+        [SerializeField] float weaponRange;
+
+        public float GetWeaponRange() => weaponRange;
+        public float GetWeaponDamage() => weaponDamage;
+
         public void Spawn(Transform handTransform, Animator animator)
         {
-            Instantiate(weaponPrefab, handTransform);
-            animator.runtimeAnimatorController = animatorOverride;
+            if (equippedPrefab != null)
+            {
+                Instantiate(equippedPrefab, handTransform);
+            }
+            if (animatorOverride != null)
+                animator.runtimeAnimatorController = animatorOverride;
         }
     }
 }
