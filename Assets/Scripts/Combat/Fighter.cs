b88e6d319fc961506df018e67bf5607764e81e08
velcronator <valacos7@gmail.com>
2025-05-06 23:43:58 +1000

    10.4 Creating Scriptable Objects

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -9,16 +9,14 @@ namespace RPG.Combat
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] float weaponDamage = 5f;
-        [SerializeField] GameObject weaponPrefab = null;
         [SerializeField] Transform handTransform = null;
-        [SerializeField] AnimatorOverrideController weaponOverride = null;
+        [SerializeField] Weapon weapon = null;
 
         Health target;
         float timeSinceLastAttack = Mathf.Infinity;
 
         private void Start()
         {
-            if(weaponPrefab == null) { return; }
             SpawnWeapon();
         }
 
@@ -41,13 +39,11 @@ namespace RPG.Combat
             }
         }
 
-        void SpawnWeapon()
+        private void SpawnWeapon()
         {
-            GameObject weapon = Instantiate(weaponPrefab, handTransform);
-            weapon.transform.localPosition = Vector3.zero;
-            weapon.transform.localRotation = Quaternion.identity;
+            if (weapon == null) return;
             Animator animator = GetComponent<Animator>();
-            animator.runtimeAnimatorController = weaponOverride;
+            weapon.Spawn(handTransform, animator);
         }
 
         private void AttackBehaviour()
