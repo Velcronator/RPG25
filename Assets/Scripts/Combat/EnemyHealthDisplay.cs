2c19129400ee1904c714d351e592a38be95432f3
Stamatis Velcronator Valacos <valacos7@gmail.com>
2025-08-15 16:27:26 +1000

    11.5 Enemy Health Display

---
+++ Assets/Scripts/Combat/EnemyHealthDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+using RPG.Attributes;
+
+namespace RPG.Combat
+{
+    public class EnemyHealthDisplay : MonoBehaviour
+    {
+        Health health;
+        Fighter fighter;
+        TextMeshProUGUI text;
+
+        private void Awake()
+        {
+            fighter = GameObject.FindWithTag("Player").GetComponent<Fighter>();
+            health = GameObject.FindWithTag("Player").GetComponent<Health>();
+            text = GameObject.Find("Enemy Health Value").GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Update()
+        {
+            if (fighter.GetTarget() == null || fighter.GetTarget().IsDead())
+            {
+                text.SetText("N/A");
+                return;
+            }
+            health = fighter.GetTarget().GetComponent<Health>();
+            text.SetText("{0:0}%", health.GetPercentage());
+        }
+    }
+}
