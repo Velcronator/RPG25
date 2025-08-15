f25b4f8eda02b0120dbf51040153c250c660e9c3
Stamatis Velcronator Valacos <valacos7@gmail.com>
2025-08-15 18:31:19 +1000

    11.7 Editing Scriptable Object Files

---
+++ Assets/Scripts/Stats/Stat.cs
@@ -0,0 +1,15 @@
+﻿namespace RPG.Stats
+{
+    public enum Stat
+    {   
+        Health,
+        ExperienceReward
+        //Strength,
+        //Defense,
+        //Intelligence,
+        //Dexterity,
+        //Speed,
+        //CriticalChance,
+        //CriticalDamage
+    }
+}
