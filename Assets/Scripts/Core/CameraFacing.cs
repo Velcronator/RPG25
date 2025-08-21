c08cdb5cc28320e37040a345f917bce0a3474df3
Stamatis Velcronator Valacos <valacos7@gmail.com>
2025-08-21 22:08:10 +1000

    12.8 In Game UI

---
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+
+namespace RPG.Core
+{
+    public class CameraFacing : MonoBehaviour
+    {
+        private Camera mainCamera;
+        private void Awake()
+        {
+            if (mainCamera == null)
+            {
+                mainCamera = Camera.main;
+            }
+        }
+        private void LateUpdate()
+        {
+            transform.forward = Camera.main.transform.forward;
+        }
+    }
+
+}
