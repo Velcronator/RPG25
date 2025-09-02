4016bcbd9afa57397f2bbfd5daa807cb80ab974d
Stamatis Velcronator Valacos <valacos7@gmail.com>
2025-09-03 08:18:42 +1000

    12.16 Disabling Empty Health Bars

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -6,10 +6,18 @@ namespace RPG.Attributes
     {
         [SerializeField] Health healthComponent = null;
         [SerializeField] RectTransform foreground = null;
-
+        [SerializeField] Canvas rootCanvas = null;
 
         void Update()
-        {
+        {   // todo optimize with event subscription
+            if (Mathf.Approximately(healthComponent.GetFraction(), 0)
+            || Mathf.Approximately(healthComponent.GetFraction(), 1))
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
+
+            rootCanvas.enabled = true;
             foreground.localScale = new Vector3(healthComponent.GetFraction(), 1, 1);
         }
     }
