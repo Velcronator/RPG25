a750fd454fb43b27a1e64260320354258b04657c
Stamatis Velcronator Valacos <valacos7@gmail.com>
2025-10-18 01:43:20 +1100

    5.6 Displaying Quest Progress

---
+++ Assets/Scripts/Quests/QuestList.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Quests
+{
+    public class QuestList : MonoBehaviour
+    {
+        [SerializeField] QuestStatus[] statuses;
+
+        public IEnumerable<QuestStatus> GetStatuses()
+        {
+            return statuses;
+        }
+    }
+}
