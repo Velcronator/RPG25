1005f6e1d599075945ae2d6bd95c0533ee197bcc
Stamatis Velcronator Valacos <valacos7@gmail.com>
2025-10-22 00:54:11 +1100

    5.8 Quest Completion

--- Assets/Scripts/Quests/QuestList.cs
+++ Assets/Scripts/Quests/QuestList.cs
@@ -6,7 +6,6 @@ namespace RPG.Quests
 {
     public class QuestList : MonoBehaviour
     {
-
         List<QuestStatus> statuses = new List<QuestStatus>();
 
         public event Action onUpdate;
@@ -22,21 +21,37 @@ namespace RPG.Quests
             }
         }
 
+        public void CompleteObjective(Quest quest, string objective)
+        {
+            QuestStatus status = GetQuestStatus(quest);
+            status.CompleteObjective(objective);
+            if (onUpdate != null)
+            {
+                onUpdate();
+            }
+        }
+
         public bool HasQuest(Quest quest)
         {
-            foreach (QuestStatus status in statuses)
-            {
-                if (status.GetQuest() == quest)
-                {
-                    return true;
-                }
-            }
-            return false;
+            return GetQuestStatus(quest) != null;
         }
 
         public IEnumerable<QuestStatus> GetStatuses()
         {
             return statuses;
         }
+
+        private QuestStatus GetQuestStatus(Quest quest)
+        {
+            foreach (QuestStatus status in statuses)
+            {
+                if (status.GetQuest() == quest)
+                {
+                    return status;
+                }
+            }
+            return null;
+        }
     }
+
 }
