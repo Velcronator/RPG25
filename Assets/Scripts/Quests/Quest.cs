dbcb6c9fa6b232bb838c23dc243b3b99e7d315fe
Stamatis Velcronator Valacos <valacos7@gmail.com>
2025-10-23 09:03:44 +1100

    5.10 Rewards & Objective Descriptions

--- Assets/Scripts/Quests/Quest.cs
+++ Assets/Scripts/Quests/Quest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using GameDevTV.Inventories;
 using UnityEngine;
 
 namespace RPG.Quests
@@ -6,7 +7,22 @@ namespace RPG.Quests
     [CreateAssetMenu(fileName = "Quest", menuName = "RPG Project/Quest", order = 0)]
     public class Quest : ScriptableObject
     {
-        [SerializeField] List<string> objectives = new List<string>();
+        [SerializeField] List<Objective> objectives = new List<Objective>();
+        [SerializeField] List<Reward> rewards = new List<Reward>();
+
+        [System.Serializable]
+        class Reward
+        {
+            public int number;
+            public InventoryItem item;
+        }
+
+        [System.Serializable]
+        public class Objective
+        {
+            public string reference;
+            public string description;
+        }
 
         public string GetTitle()
         {
@@ -18,14 +34,21 @@ namespace RPG.Quests
             return objectives.Count;
         }
 
-        public IEnumerable<string> GetObjectives()
+        public IEnumerable<Objective> GetObjectives()
         {
             return objectives;
         }
 
-        public bool HasObjective(string objective)
+        public bool HasObjective(string objectiveRef)
         {
-            return objectives.Contains(objective);
+            foreach (var objective in objectives)
+            {
+                if (objective.reference == objectiveRef)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public static Quest GetByName(string questName)
