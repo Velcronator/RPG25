b55a099fd390989a2423f9cc829f7157bad6edf5
Stamatis Velcronator Valacos <valacos7@gmail.com>
2025-10-29 22:44:35 +1100

    1.13 Displaying A ShopItem

--- Assets/Scripts/Shops/Shop.cs
+++ Assets/Scripts/Shops/Shop.cs
@@ -19,7 +19,7 @@ namespace RPG.Shops
             yield return new ShopItem(InventoryItem.GetFromID("135bd571-f3cb-462b-80fd-7816762360d3"),
                 10, 10.0f, 0);
             yield return new ShopItem(InventoryItem.GetFromID("0935e90f-0534-4d28-a882-2fa50115ce9a"),
-                10, 10.0f, 0); 
+                10, 1000.99f, 0); 
             yield return new ShopItem(InventoryItem.GetFromID("0935e90f-0534-4d28-a882-2fa50115ce9a"),
                 10, 10.0f, 0);
         }
