85e8881d71407138ef215dea29ac4d2444ab485e
Stamatis Velcronator Valacos <valacos7@gmail.com>
2025-10-30 13:29:58 +1100

    1.15 Quantity Widget

--- Assets/Scripts/UI/Shops/RowUI.cs
+++ Assets/Scripts/UI/Shops/RowUI.cs
@@ -7,17 +7,36 @@ namespace RPG.UI.Shops
 {
     public class RowUI : MonoBehaviour
     {
-        [SerializeField] Image iconField;
-        [SerializeField] TextMeshProUGUI nameField;
-        [SerializeField] TextMeshProUGUI availabilityField;
-        [SerializeField] TextMeshProUGUI priceField;
+        [SerializeField]
+        Image iconField;
+        [SerializeField]
+        TextMeshProUGUI nameField;
+        [SerializeField]
+        TextMeshProUGUI availabilityField;
+        [SerializeField]
+        TextMeshProUGUI priceField;
 
-        public void Setup(ShopItem item)
+        Shop currentShop = null;
+        ShopItem item = null;
+
+        public void Setup(Shop currentShop, ShopItem item)
         {
+            this.currentShop = currentShop;
+            this.item = item;
             iconField.sprite = item.GetIcon();
             nameField.text = item.GetName();
             availabilityField.text = $"{item.GetAvailability()}";
             priceField.text = $"${item.GetPrice():N2}";
+        }
+
+        public void Add()
+        {
+            currentShop.AddToTransaction(item.GetInventoryItem(), 1);
+        }
+
+        public void Remove()
+        {
+            currentShop.AddToTransaction(item.GetInventoryItem(), -1);
         }
     }
 }
