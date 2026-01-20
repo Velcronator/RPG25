b45c3bcd6e2d8d5dd36d725c5c0eeab76544ad1c
Stamatis Velcronator Valacos <valacos7@gmail.com>
2026-01-20 18:43:05 +1100

    4.6 New Game & PlayerPrefs

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -3,11 +3,14 @@ using RPG.SceneManagement;
 using UnityEngine.UI;
 using GameDevTV.Utils;
 using System;
+using TMPro;
 
 namespace RPG.UI
 {
     public class MainMenuUI : MonoBehaviour
     {
+
+        [SerializeField] TMP_InputField newGameNameField;
         LazyValue<SavingWrapper> savingWrapper;
 
         private void Awake()
@@ -23,6 +26,11 @@ namespace RPG.UI
         public void ContinueGame()
         {
             savingWrapper.value.ContinueGame();
+        }
+
+        public void NewGame()
+        {
+            savingWrapper.value.NewGame(newGameNameField.text);
         }
     }
 }
